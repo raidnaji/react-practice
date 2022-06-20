0ff641f1352e43e4cd8f356fa0861a065cf8bfbb
Naji <naji.raid@gmail.com>
2022-06-20 16:13:38 -0400

    order app final commit

--- order-app/src/App.js
+++ order-app/src/App.js
@@ -1,16 +1,28 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import { useState } from "react";
+import CartProvider from "./store/cartProvider";
 
 function App() {
+  const [cartIsShown, SetCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    SetCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    SetCartIsShown(false);
+  };
+
   return (
-    <>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
