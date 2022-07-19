092454a72bc0dfd5eda212bf52f6bdfb883bd3eb
Naji <naji.raid@gmail.com>
2022-07-19 11:34:50 -0400

    wip

--- typescript/basics.ts
+++ typescript/basics.ts
@@ -49,3 +49,19 @@ function add(a: number, b: number) {
 function printOutput(value: any) {
     console.log(value);
 }
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray
+}
+
+const demoArray = [1, 2, 3]
+
+const updatedArray = insertAtBeginning(demoArray, -1);
+
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd')
+
+// updatedArray[0].split(""); // calling split is not possible here because split can only be called on strings and typescript knows that everything in the array are numbers.
+
